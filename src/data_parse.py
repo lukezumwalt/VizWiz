87730b352e62c3461259f0c7fd7c77277486e7ae
Luke <luzu8839@colorado.edu>
2025-03-16 22:44:15 -0600

    Defines a data struct class for any data set with metadata reporting and visualizing by index and richness

--- src/data_parse.py
+++ src/data_parse.py
@@ -4,8 +4,8 @@ data_parse.py
 Reads and interprets data and metadata of
 the 2018 VizWiz data set.
 
-3/16/25
 Lukas Zumwalt
+3/16/25
 '''
 
 import json
@@ -16,9 +16,9 @@ import matplotlib.pyplot as plt
 # Directory for all images
 IMG_DIR = '../data/images/'
 
-TRAIN_IMG_DIR = IMG_DIR + 'train/'
-VAL_IMG_DIR = IMG_DIR + 'val/'
-TEST_IMG_DIR = IMG_DIR + 'test/'
+TRAIN_IMG_PATH = IMG_DIR + 'train/'
+VAL_IMG_PATH = IMG_DIR + 'val/'
+TEST_IMG_PATH = IMG_DIR + 'test/'
 
 # Directory for annotation files
 ANN_DIR = '../data/annotations/'
@@ -27,13 +27,68 @@ TRAIN_ANNOTATION_PATH = f'{ANN_DIR}train.json'
 VAL_ANNOTATION_PATH = f'{ANN_DIR}val.json'
 TEST_ANNOTATION_PATH = f'{ANN_DIR}test.json'
 
-# Plot image from the image url
-def visualize_image(image_path):
-    image = io.imread(image_path)
-    print(image_path)
-    plt.imshow(image)
-    plt.axis("off")
-    plt.show()
+
+class DataStruct():
+    '''
+    Dataset class designed to hold pointers
+    for respective data in accessible member form.
+    '''
+
+    def __init__(self, images, annotations, txform=None):
+        self.txform = txform
+        self.image_path = images
+        self.annotation_path = annotations
+        with open(self.annotation_path, 'r', encoding='utf-8') as fn:
+            self.annotations = json.load(fn)
+
+    def __len__(self):
+        return len(self.image_path)
+
+    def __getitem__(self,idx):
+        image = self.image_path[idx]
+        annotation = self.annotation_path[idx]
+
+        if self.txform:
+            image = self.txform(image)
+
+        # Tuple return
+        return image, annotation
+
+    def show(self, idx, rich=False):
+        '''
+        Method to depict an indexed image and more metadata.
+        '''
+        sample = self.annotations[idx]
+        if rich:
+            print('Sample:\n', sample)
+        sample_image_name = sample['image']
+        sample_image_path = self.image_path + sample_image_name
+        sample_question = sample['question']
+        sample_answers = sample['answers']
+        sample_label = sample['answerable']
+        # Printing
+        print('Image name (file name):', sample_image_name)
+        print('Question:', sample_question)
+        if rich:
+            print('Answers:')
+            for _,n in enumerate(sample_answers):
+                print(n)
+        else:
+            print('Answer index 0:', sample_answers[0])
+        print('Answerability label:', sample_label)
+        # View image sample
+        self.visualize_image(sample_image_path)
+
+    # Plot image from the image url
+    def visualize_image(self, image_path):
+        '''
+        Call to read image path and depict.
+        '''
+        image = io.imread(image_path)
+        print(image_path)
+        plt.imshow(image)
+        plt.axis("off")
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -54,25 +109,5 @@ if __name__ == '__main__':
     print('Validation set size:', len(val_data))
     print('Test set size:', len(test_data))
 
-    vq = train_data[0]
-
-    # Try printing the entire set of annotation to see the structure
-    print('First sample:', vq)
-
-    # Unpack data: image
-    image_name = vq['image']
-    image_path = TRAIN_IMG_DIR + image_name
-    print('Image URL:', image_path)
-
-    # Unpack data: question
-    question = vq['question']
-
-    # Unpack data: labels
-    answers = vq['answers']
-    label = vq['answerable']
-    print('Image name (file name):', image_name)
-    print('Question:', question)
-    print('Answer index 0:', answers[0])
-    print('Answerability label:', label)
-
-    visualize_image(image_path)
+    training_data = DataStruct(TRAIN_IMG_PATH, TRAIN_ANNOTATION_PATH, None)
+    training_data.show(1,True)
